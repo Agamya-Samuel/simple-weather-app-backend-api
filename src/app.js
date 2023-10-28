e78ff2b238834cac2b7ab679e9dc38fbe805b51d
Agamya Samuel <48693970+Agamya-Samuel@users.noreply.github.com>
2023-10-28 10:52:54 +0530

    added CORS Policy
    - now you can allow only specific domains to access the Backend API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 // importing express
 const express = require('express');
 
+// importing 'cors' for 'strict-origin-when-cross-origin'
+const cors = require('cors');
+
 // importing .env
 require('dotenv').config();
 
 PORT = process.env.PORT;
+CORS_ALLOWED_ORIGINS = process.env.CORS_ALLOWED_ORIGINS;
 
 // initializing express app
 const app = express();
+
+// use cors middleware
+const allowedOrigins = CORS_ALLOWED_ORIGINS;
+
+// use cors middleware
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error('Not allowed by CORS'));
+			}
+		},
+	})
+);
 
 // make sever accept payload in json format, using middleware
 app.use(express.json());
